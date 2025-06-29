b246c666f47fbd6e8c3f8214f74a7d6909df7d8b
everson1910 <4557087@alunouninter.com>
2025-06-29 12:35:20 -0300

    Changing Level

--- code/Const.py
+++ code/Const.py
@@ -1,7 +1,7 @@
 # c
 COLOR_WHITE = (255, 255, 255)
 COLOR_RED =  (255, 0, 0)
-
+COLOR_YELLOW =(255, 255, 0)
 # M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
--- code/Menu.py
+++ code/Menu.py
@@ -4,7 +4,7 @@ import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.const import WIN_WIDTH, MENU_OPTION, COLOR_WHITE, COLOR_RED
+from code.Const import WIN_WIDTH, MENU_OPTION, COLOR_WHITE, COLOR_RED, COLOR_YELLOW
 
 
 class Menu:
@@ -14,17 +14,20 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
+        menu_option = 0
         pygame.mixer_music.load('./asset/Menu.mp3')
         pygame.mixer_music.play(-1)
         while True:
+            # DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text( 50, "DEA", COLOR_RED, ((WIN_WIDTH / 2), 70))
-            self.menu_text( 50, "CEMETERY", COLOR_RED, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, "DEA", COLOR_RED, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "CEMETERY", COLOR_RED, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text( 20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
-            pygame.display.flip()
-
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
 
             pygame.display.flip()
 
@@ -33,6 +36,19 @@ class Menu:
                 if event.type == pygame.QUIT:
                     pygame.quit()  # Close Window
                     quit()  # end pygame
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:  # TECLA PARA BAIXO
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP:
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN:
+                        return MENU_OPTION[menu_option]
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Tempus Sans ITC", size=text_size)
