da4c220fc09586a1ee6c018de8182605da6ea813
everson1910 <4557087@alunouninter.com>
2025-06-29 17:57:25 -0300

    Changing Level

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-class EntityFactory:
-    def __init__(self):
-        pass
+from code.Background import Background
 
-    def get_entity(self, entity_type):
-        pass
+
+class EntityFactory:
+
+    @staticmethod
+    def get_entity(entity_name: str, position=(0,0)):
+        match entity_name:
+            case 'Level1Bg':
+                list_bg = []
+                for i in range(7):
+                    list_bg.append(Background(f'Level1Bg{i}', position(0,0)))
+                    return list_bg
