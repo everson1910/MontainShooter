e96bf56354d28f815bc259ccdc532a0e15fd35bb
everson1910 <4557087@alunouninter.com>
2025-07-05 17:00:55 -0300

    Creating shot player and enemy

--- code/PlayerShot.py
+++ code/PlayerShot.py
@@ -5,7 +5,8 @@ from code.Entity import Entity
 class PlayerShot(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
-
+        self.rect = self.surf.get_rect(center=position)
 
     def move(self, ):
         self.rect.centerx += ENTITY_SPEED[self.name]
+
